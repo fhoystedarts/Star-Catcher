dd71a58d48f79a3c78e1b3c54ebb41c58910bbb6
Faith Hoysted <fhoystedarts@gmail.com>
2020-02-14 09:11:37 -0500

    First Commit
    Putting the project on GitHub

---
+++ Assets/Scripts/Notes2.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //this lets you call IMages that are used in UI
+using UnityEngine.SceneManagement; //lets you call scenemanager which loads levels
+using UnityEngine.Audio;
+
+public class Notes2 : MonoBehaviour
+{
+    //Faith Hoysted
+    public Notes2 n2;
+    public GameObject GO;
+    public Image myimage;
+    public Button mybutton;
+    public float timer;
+    // Start is called before the first frame update
+    //AI
+    // MOVE TOWARDS (moves object towards another)
+    public Vector3 start; // start locations
+    public Vector3 finish; //the end location or target
+    public float speed; // how fast the mover goes from start to finish
+    //LOOT
+    public GameObject money;
+
+    //ARRAY an array is a collection of gameObjects. Arrays allow us to affect large groups of objects
+    public GameObject[] alltheEnemies; //the straight brackets signify a collection of gameObjects
+
+    //PLAYER PREFS
+    //these are values that will save even after the player leaves the game.  this is good for unlockable content or overall progress
+    //also works for character skins
+
+    public int GreenBird; // this is not a player pref just a normal int
+
+
+    void Start()
+    {
+        PlayerPrefs.SetInt("GreenBird", 2); //this will unlock the green bird so I would only do this when I am ready to unlock it.
+        //befor this it would be set to 0 or 1;
+
+        if(PlayerPrefs.HasKey("GreenBird"))
+        {
+            GreenBird = PlayerPrefs.GetInt("GreenBird"); // if I beat the game it will be 2
+            //if I didn't beat the game yet it will be 1
+        }
+
+        if(GreenBird == 2)
+        {
+            // I can select the green bird
+        }
+
+        alltheEnemies = GameObject.FindGameObjectsWithTag("enemy"); // this grabs all the enemies
+        //ENABLED VS SET ACTIVE
+        //we enabled and disable components attached to gameObjects
+        // we set active true or false entire gameObjects
+
+        n2.enabled = true; // enables the notes2 script (any component added)
+        GO.SetActive(true); // enabled the entire gameobject
+        StartCoroutine(StarPower()); //this line calls the IEnumerator (don't do this in update)
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TIMERS
+        //We can create timers using the command Time.deltaTime.
+        //This means time that has pass since the last frame (so a constant timer).
+        //We can take a float and add or subtract Time.dltaTime to create a timer that counts up or down
+        timer -= Time.deltaTime; // -= subtracts time from the timer consistently
+        timer += Time.deltaTime; // += adds time to the timer
+
+        if (timer >= 0)
+        {
+            mybutton.interactable = true; // this allows the button to be pressed
+        }
+
+        if (timer <= 0)
+        {
+            mybutton.interactable = false; //this greys the button out and cannot be pressed
+        }
+
+        Vector3.MoveTowards(start, finish, speed); //this line will move the object
+        //the start poisiton is set to the movers position
+        //target position gets set to another gameobject (usually player)
+        //speed is constant
+
+        //INSTANTIATE is a way to make things appear in our scene, this could be enimes, loot or power ups
+
+        GameObject loot = Instantiate(money, transform.position, transform.rotation);
+        //above line makes a new gameobject by creating money at our current location
+        //we say GameObject loot because we are creating a new gameobject in the hierarchy called loot
+        //money is usually a prefab that we can call over and over again
+        //this means money won't be in the hierarchy but inside the game's asset folder
+        Destroy(money, 5f); //this destroys the money if we don't get it fast enough
+
+        //AUTOPOOLING we are using a pluging called Mob Farm Auto Pooler
+        GameObject loot2 = MF_AutoPool.Spawn(money, transform.position, transform.rotation);
+        //this is the mobile friendly version
+        MF_AutoPool.Despawn(this.gameObject, 5f); //this would be on the money itself and would put it back into the pool without any left over issues
+
+        //ARRAY (How to go through your list and affect each object one at a time)
+        for (int i = 0; i < alltheEnemies.Length; i++)
+        {
+            alltheEnemies[i].SetActive(true); // we set each enemy active
+        }
+        // int i = 0 means we start at the top of the list
+        //i < alltheEnemies, means as long as the number we are on isn't larger that the list
+        //lets say the list is 10 long if we are at 0, we add 1, 1 is less that 10 so we add another so on until we get to 10
+        //i++ we move down the list by one
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //this statement is used when we want the player to walk into a specific area and trigger something.
+        //This could be an animation, enemy, really anything
+        //an event is triggered when the player passes through a collider that is marked as a trigger.
+        //either player or collider must have a rigidbody in order for the hit to register.
+
+        if(collision.tag == "Player")
+        {
+            //Then we trigger the event. This will only trigger if the collider has the tag player, so enemies can't trigger this event.
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //unlike a trigger a collision happens when two colliders bump into each other but do not pass through
+    }
+
+    //CO-ROUTINE  which unity calls IEnumerator
+    //normal functions in unity are read top to bottom all at once.
+    //sometimes we want to pause before finishing the function which is when we use IEnumerator
+    //think of this as the star power in SMB, when you get the star the player is invincible for the short time, sprite changes,
+    //music changes and then eventually it wears off
+
+    public IEnumerator StarPower ()
+    {
+        //give mario star power
+        //change music
+        //change sprite graphics
+        //then we want to wait
+        yield return new WaitForSeconds(5f); //this line makes Unity wait for 5 seconds
+        //star power wears off
+        //change music back
+        //change sprite back
+    }
+
+    //to call an Ienumerator we use the word Co-Routine (see Start loop)
+}
---
+++ Assets/Scripts/cSharpNotes.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cSharpNotes : MonoBehaviour
+{
+    //Faith Hoysted
+    //Two foward slashes allows for comments these are not read by the engine
+    //The top part of the script is generally where we store the variables.  Things like numbers, references to objects or references to other scrpits.
+    //Variables have three parts, the 1st part is public or private, the second part is the type of variable and the third part is whatever we decide to name the variable
+
+    //NUMBER VARIABLES
+   //There are 2 common types of number variables floats and ints
+   public float number; //float stands for floating point number which means the number has a decimal point
+    public int wholenumber; // 1, 2, 3 are ints
+    private float myhiddennumber; //a private variable is not visible instide the inspector
+
+    //BOOLS (true/ false statements)
+    public bool yesorno; // a bool is a yes or no statement, a binary, thing of it like a light switch, either on or off, no in between.
+
+    //other common variables
+    public GameObject mygameObject; // we can reference any object in our scene, all iteams in the heirarchy are considered gameobjects.
+    public cSharpNotes csn; //we can also reference any script that have written as long as its public
+    public Rigidbody2D myRB2D; // we use rigidbodies on players and enemies and anything we want to be affected by unity's physics
+    public BoxCollider2D myboxcollider; //we can also reference colliders of all types
+    public CircleCollider2D mycirclecollider;
+    //We usually put these recerences at the top of the script we need to call them here first if we want to manipulate them later in the script
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //anything you want to happen when the game starts goes in here
+        //sometimes we don't want to have to manually drag and drop items in the inspector
+        //sometimes we want to spawn new items during gameplay. In this case we can use a few simple commands to have the script autuomatically find objects
+
+        myRB2D = GetComponent<Rigidbody2D>(); // this will get the rigidbody but only if its on the same object as our script
+        myRB2D = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        //this will find any object in our scene that has the tag player and get its rigidbody
+        myRB2D = GameObject.FindObjectOfType<Rigidbody2D>();
+        //this only works when there is no more than one rigidbody
+
+        //when the game starts we also might want to look at a few different properties of our gameobjects, transform, position, rotation and scale
+        transform.position = new Vector2(0, 0);
+        //transform position is our location on x and y. transforms are ready by unity as something called a Vector (Vector 2 or Vector 3) Think
+        //of a vector like a bar graph, the x is the horizontal and the y is the vertical
+        //the vector 2 above is at origin position. another example would be
+        transform.position = new Vector2(24, 128); //this would move us 24 units right and 126 units up
+        //we can also manipulate scale this way
+        transform.localScale = new Vector2(0, 0); //2D
+        transform.localScale = new Vector3(0, 0, 0); //3D both these scales would be invisible
+        //roation is a little more complicated we use quaternion and EULER (oiler)
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // inside the update function we call things that we want to update in real time as we play the game
+
+        //IF STATEMENTS
+        if (yesorno == true)
+        {
+            //we say yes
+            //the player lives
+        }
+
+        if(yesorno == false)
+        {
+            //we say no
+            //player dies
+        }
+
+        //this is also an example of how bools can work. if the bool is true one thing happens
+        //if the bool is false something else happens
+        //for the if statement to work we need a double = sign a single = means that the bool is true or is false whereas a double = checks to see if
+        //its true or false
+
+        if(number >= 10)
+        {
+            //we do something
+        }
+
+        // we can also use if statements to control the player
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            //we nive the player
+            myRB2D.velocity = new Vector2(25, 0);
+        }
+
+        if (Input.GetAxis("Horizontal") == 0)
+        {
+            //we want to stop the player
+            myRB2D.velocity = new Vector2(0, 0); //this is a zero velocity
+            //to see all the different rigidbody options we have just start typing myRB2D.
+            myRB2D.gravityScale = 0.5f; //gives me half gravity
+            myRB2D.simulated = false; // this means the rigid body is no longer affected by physics engine
+            myRB2D.isKinematic = true; // kinematic rigidbody only moves if the code tells it to
+            myRB2D.isKinematic = false; // non kinematic is the same as dynamic which means it is affected by the physics engine.
+        }
+        // IF ELSE STATEMENTS
+        //if statements get read one after the other which can sometimes cause weird behavior
+        //we can avoid this by using IF ELSE statements
+        if (yesorno == true)
+        {
+            //we say yes
+            //the player lives
+            //if this turns out to be true we won't read the below else statement
+        }
+
+        else if (yesorno == false)
+        {
+            //we say no
+            //player dies
+            //if the above if statement is not true we will read this else statement
+        }
+        //because this code is in the update loop changes can happen quickly and we can cycle through multiple if statements faster than we want
+        //this is why we use else
+    }
+
+    public void FixedUpdate()
+    {
+       //regular update is based on frame rate which means that a newer computer will run the code faster and an older computer will run the code slower
+       //this is not ideal
+       //for the most part graphical elements can live inside the update loop without issue
+       //the fixed update loop is used for physics calculations as its called on a set interval which means that all computers run the code at the same speed
+    }
+}
