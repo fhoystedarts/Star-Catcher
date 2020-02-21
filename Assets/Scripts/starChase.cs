7d65bb0f3a39b795d84b39e76eaae8aa44abfad9
Faith Hoysted <fhoystedarts@gmail.com>
2020-02-21 09:27:59 -0500

    Added AutoPool

---
+++ Assets/Scripts/starChase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starChase : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//the screen has been touched
+
+		Touch touch = Input.GetTouch(0);
+
+		if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+		{
+
+			Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0.0f));
+
+			transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, touchPosition.y, transform.position.z), Time.deltaTime * 5.0f);
+
+			//attatch to net model
+			//enemy spawner 
+			//meteors would have greater up velocity. Set up spawner 
+			//for differnt nets, change how "powerful" the meteors are for each new new
+
+
+
+		}
+	}
+}
